020f1a85251fa2282f45241bbde1679672ae0bd3
ViniciosLugli <vinicioslugli@gmail.com>
2023-11-23 22:02:25 -0300

    feat(frontend): :sparkles: Added assets for the project

--- src/plataform/plataform/settings.py
+++ src/plataform/plataform/settings.py
@@ -167,6 +167,9 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
