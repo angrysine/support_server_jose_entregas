5ee55b2c3e62b874974b4236c0cefac6a882ed57
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-12 17:33:45 -0300

    feat: :card_file_box: Added log table

--- src/plataform/common/models.py
+++ src/plataform/common/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from enum import IntEnum
 
-# Create your models here.
+
+class Log(models.Model):
+    class Status(IntEnum):
+        REJECTED = 0
+        PENDING = 1
+        APPROVED = 2
+
+        @classmethod
+        def choices(cls):
+            return [(key.value, key.name) for key in cls]
+
+    requester_name = models.CharField(max_length=100)
+    requester_number = models.CharField(max_length=100)
+    item = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=Status.choices(), default=Status.PENDING)
