d8650e572e28ccaec449d9b999bc40208475bb87
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2023-12-13 18:18:16 -0300

    perf(stt): :zap: change stt from whisper to openai api

--- src/bridge/stt.py
+++ src/bridge/stt.py
@@ -1,45 +1,27 @@
 from decouple import config
-import whisper
+from openai import OpenAI
 
-# from openai import OpenAI
-
-
-# api_key = config("OPENAI_API_KEY")
-
-# client = OpenAI(api_key=api_key)
-
+api_key = config("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
 class STT:
     def __init__(self, filename=None):
-        self.model = whisper.load_model("medium")
         self.filename = filename
-
+        self.audio_file = open(self.filename, "rb")
 
     def transcribe(self):
         if self.filename is not None:
-            # load audio and pad/trim it to fit 30 seconds
-            audio = whisper.load_audio(self.filename)
-            audio = whisper.pad_or_trim(audio)
-
-            # make log-Mel spectrogram and move to the same device as the model
-            mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
-
-            # detect the spoken language
-            _, probs = self.model.detect_language(mel)
-            print(f"Detected language: {max(probs, key=probs.get)}")
-
-            # decode the audio
-            options = whisper.DecodingOptions(fp16=False)
-            result = whisper.decode(self.model, mel, options)
-            return result.text
-
+            transcript = client.audio.transcriptions.create(
+            model="whisper-1", 
+            file=self.audio_file, 
+            response_format="text"
+            )
+            return transcript
 
 def main():    
     # arquivo só para teste, excluir dps
     stt = STT(filename='./audio/audio.ogg')
-    print(stt)
     speech_text = stt.transcribe()
-    
     print(speech_text)
 
 if __name__ == "__main__":
