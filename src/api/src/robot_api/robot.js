2632932a5a739e65b0bbbc2b967ed4af80fb0f4e
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-12-14 20:27:00 -0300

    UPDATE_API-clear_code

--- src/api/src/robot_api/robot.js
+++ src/api/src/robot_api/robot.js
@@ -6,13 +6,15 @@ rclnodejs.init()
 const node = rclnodejs.createNode('client');
 const publisher = node.createPublisher('std_msgs/msg/String', 'llm_topic');
 
+//api_topic
+
 function send(msg) {
     try {
         publisher.publish(`${msg}`);
         const spawn = require("child_process").spawn;
         const pythonProcess = spawn('python3',[path.resolve(__dirname, 'tts.py'), msg]);
         return "Mensagem após cliente send";
- 
+
     } catch (error) {
         console.log(error)
     }
